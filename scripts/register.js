b5729f79626ed0ef2d9e96dd386bcc390992923e
edwardtimsack <edwardtimothysackie@gmail.com>
2023-12-29 23:33:24 +0000

    updated mobile responsive login and register pages

--- scripts/register.js
+++ scripts/register.js
@@ -1,9 +1,8 @@
-const burgerMenu = document.querySelector('.ham');
+const burgerMenu = document.querySelector('.ham-burger-menu');
 const navigation = document.querySelector('nav');
 const closeIcon = document.querySelector(".menu-close");
 const titleNavigation =document.querySelector(".title-navigation")
 const hideMain = document.querySelector(".main")
-const filterDiv = document.querySelector('.filter-button')
 
 burgerMenu.addEventListener('click', displayNav);
 closeIcon.addEventListener('click', closeNav);
@@ -11,14 +10,15 @@ closeIcon.addEventListener('click', closeNav);
 function displayNav() {
     navigation.style.display = "block";
     titleNavigation.style.display = 'none';
-    // hideMain.style.display = 'none';
+    hideMain.style.display = 'none';
 }
 
 function closeNav() {
-    titleNavigation.style.display = 'flex'
     navigation.style.display = 'none';
-    // filterDiv.style.display = 'none'
-    burgerMenu.style.display = 'block'
-    // hideMain.style.display = 'flex';
-    // titleNavigation.style.display = 'block';
+    hideMain.style.display = 'flex';
+    titleNavigation.style.display = 'block';
 }
+
+// function hideTitle() {
+//     hideMain.style.display = 'none';
+// }
